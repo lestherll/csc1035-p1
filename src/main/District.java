b593678a84fd41ed28ee6d60b11eee595a680e5e
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-09 17:05:30 +0000

    Added District class

---
+++ src/main/District.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.*;
+
+public class District {
+    private String name;
+    private List<Incident> incidents = new ArrayList<>();
+
+    public District(String name) {
+        this.name = name;
+    }
+
+    public District(String name, List<Incident> incidents) {
+        this.name = name;
+        this.incidents = incidents;
+    }
+
+    public District(String name, Incident[] incidents) {
+        this.name = name;
+        this.incidents = Arrays.asList(incidents);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Incident> getIncidents() {
+        return this.incidents;
+    }
+
+    @Override
+    public String toString() {
+        return "District{" +
+                "name='" + name + '\'' +
+                ", incidents=" + incidents +
+                '}';
+    }
+}
