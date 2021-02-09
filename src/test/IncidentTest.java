5c7df70041d5b8928184f100323b35e56e41084e
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-09 16:56:44 +0000

    Added test method for setters

--- src/test/IncidentTest.java
+++ src/test/IncidentTest.java
@@ -16,4 +16,21 @@ class IncidentTest {
         assertEquals(20.0, incident.getValue());
     }
 
+    @Test
+    public void testSetters() {
+        Incident incident = new Incident();
+
+        incident.setPostCode("TS5");
+        assertEquals("TS5", incident.getPostCode());
+
+        incident.setMonth(12);
+        assertEquals(12, incident.getMonth());
+
+        incident.setYear(2020);
+        assertEquals(2020, incident.getYear());
+
+        incident.setValue(100);
+        assertEquals(100, incident.getValue());
+    }
+
 }
