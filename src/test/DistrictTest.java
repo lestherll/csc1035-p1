d1cf3c8b30526a632fff0ad71feae2913488e460
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-09 18:34:22 +0000

    Added test setup for District

---
+++ src/test/DistrictTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DistrictTest {
+
+    @Test
+    void testGetIncidents() {
+    }
+
+    @Test
+    void testAddIncident() {
+    }
+
+    @Test
+    void testGetAverageVal() {
+    }
+
+    @Test
+    void testGetHighestVal() {
+    }
+
+    @Test
+    void testGetIncidentWithValGreaterThan() {
+    }
+}
