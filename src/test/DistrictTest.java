a1ca596112093d4ef74195dc0d41173229b9a1dc
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-10 00:27:03 +0000

    Test case for District instantiator and incident adder

--- src/test/DistrictTest.java
+++ src/test/DistrictTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import main.District;
+import main.Incident;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +10,21 @@ class DistrictTest {
 
     @Test
     void testGetIncidents() {
+        Incident incident = new Incident("NE1", 12, 2020, 250.0);
+        Incident incident1 = new Incident("NE2", 1, 2021, 100.0);
+        District district = new District("Newcastle", new Incident[] {incident, incident1} );
+        assertEquals(2, district.getIncidents().size());
+        assertEquals(incident, district.getIncidents().get(0));
+        assertEquals(incident1, district.getIncidents().get(1));
     }
 
     @Test
     void testAddIncident() {
+        Incident incident = new Incident("WC1", 2, 2021, 1500.45);
+        District district = new District("London");
+        district.addIncident(incident);
+        assertEquals(1, district.getIncidents().size());
+        assertEquals(incident, district.getIncidents().get(0));
     }
 
     @Test
