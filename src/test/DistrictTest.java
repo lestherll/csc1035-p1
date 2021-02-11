8350a35e253174b7ceba1c0dc52187aa2887717c
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-11 16:54:30 +0000

    Added test case for District.getAverageVal

--- src/test/DistrictTest.java
+++ src/test/DistrictTest.java
@@ -4,6 +4,9 @@ import main.District;
 import main.Incident;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DistrictTest {
@@ -25,14 +28,38 @@ class DistrictTest {
         district.addIncident(incident);
         assertEquals(1, district.getIncidents().size());
         assertEquals(incident, district.getIncidents().get(0));
+
+        List<Incident> incidents = new ArrayList<>();
+        incidents.add(new Incident());
+        incidents.add(new Incident());
+
+        district.addIncident(incidents);
+        assertEquals(3, district.getIncidents().size());
+
+        district.addIncident(new Incident[] {new Incident(), new Incident()});
+        assertEquals(5, district.getIncidents().size());
+
     }
 
     @Test
     void testGetAverageVal() {
+        District district = new District("Newcastle");
+
+        assertEquals(0, district.getAverageVal());
+
+        Incident incident = new Incident("NE1", 12, 2020, 250.0);
+        Incident incident1 = new Incident("NE2", 1, 2021, 100.0);
+        district.addIncident(incident);
+        district.addIncident(incident1);
+
+        double expected = (incident.getValue() + incident1.getValue())/2;
+
+        assertEquals(expected, district.getAverageVal());
     }
 
     @Test
     void testGetHighestVal() {
+
     }
 
     @Test
