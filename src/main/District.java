797ddfb92162b54c1e36c3232cc4f8470c6555a3
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-11 20:42:40 +0000

    Added District.setIncidents setter

--- src/main/District.java
+++ src/main/District.java
@@ -39,6 +39,10 @@ public class District {
                 '}';
     }
 
+    public void setIncidents(List<Incident> incidents) {
+        this.incidents = incidents;
+    }
+
     public void addIncident (Incident incident) {
         this.incidents.add(incident);
     }
