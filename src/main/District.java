1cddd319aa9352f5a8fbaaafe4e4c28abf8a00ea
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-13 21:07:36 +0000

    Javadoc annotations for District class

--- src/main/District.java
+++ src/main/District.java
@@ -2,14 +2,28 @@ package main;
 
 import java.util.*;
 
+/**
+ * District represents the class that will contain all Incidents
+ * that are located in the same region or area. A district will
+ * have a name and the list of incidents.
+ */
 public class District {
     private String name;
     private List<Incident> incidents = new ArrayList<>();
 
+    /**
+     * Construct a district only with a name
+     * @param name The name of the district
+     */
     public District(String name) {
         this.name = name;
     }
 
+    /**
+     * Construct a district with name and incidents
+     * @param name the name of the District
+     * @param incidents the list of incidents to be added to District
+     */
     public District(String name, List<Incident> incidents) {
         this(name);
         this.incidents.addAll(incidents);
@@ -39,6 +53,11 @@ public class District {
                 '}';
     }
 
+    /**
+     * Checks if two districts are equal: same name and class
+     * @param other The other object that this object is being compared to
+     * @return boolean value of the comparison
+     */
     @Override
     public boolean equals(Object other) {
         if (other instanceof District) {
@@ -51,18 +70,35 @@ public class District {
         this.incidents = incidents;
     }
 
+    /**
+     * Add a single incident to this.incidents field
+     * @param incident an incident to be added to list of all incidents
+     */
     public void addIncident (Incident incident) {
         this.incidents.add(incident);
     }
 
+    /**
+     * A multiple incidents in List form to this.incidents field
+     * @param incidents the incidents that will be added
+     */
     public void addIncident (List<Incident> incidents) {
         this.incidents.addAll(incidents);
     }
 
+    /**
+     * Add multiple incidents in Array form to this.incidents
+     * @param incidents the incidents that will be added
+     */
     public void addIncident (Incident[] incidents) {
         this.addIncident(Arrays.asList(incidents));
     }
 
+    /**
+     * Average value of incidents in a given year
+     * @param year Year wanted for the average
+     * @return the value of the average
+     */
     public double getAverageValAt(int year) {
         if (this.incidents.size() == 0) {
             return 0;
@@ -77,6 +113,10 @@ public class District {
         return total/incidents.size();
     }
 
+    /**
+     * Greatest value stolen in an Incident from a district
+     * @return the greatest value stolen
+     */
     public Incident getHighestVal() {
         if (this.incidents.size() == 0) {
             return null;
@@ -97,6 +137,11 @@ public class District {
         return highest;
     }
 
+    /**
+     * Incidents with greater value than the given amount
+     * @param val the value that will be used as reference
+     * @return the list of Incidents with greater value than the given amount
+     */
     public List<Incident> getIncidentWithValGreaterThan(double val) {
         List<Incident> valArr = new ArrayList<>();
         for (Incident incident: this.incidents) {
