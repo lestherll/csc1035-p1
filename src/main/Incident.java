4a9266b7815abada7ae7fe8d677b6aad19326d12
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-13 20:26:45 +0000

    Javadoc annotations for Incident class

--- src/main/Incident.java
+++ src/main/Incident.java
@@ -1,5 +1,12 @@
 package main;
 
+/**
+ * The Incident class represents burglaries that occur.
+ * It records where it happens, using a postcode, when
+ * it happens using month and year, and how much was
+ * stolen. This is the backbone of every other class
+ * in this program.
+ */
 public class Incident {
     private String postCode;
     private int month;
@@ -8,6 +15,13 @@ public class Incident {
 
     public Incident() {}
 
+    /**
+     *
+     * @param postCode the postcode where the incident happened
+     * @param month the month(int) when in happened
+     * @param year the year(int) the incident happened
+     * @param value the amount of value that was stolen
+     */
     public Incident(String postCode, int month, int year, double value) {
         this.postCode = postCode;
         this.month = month;
