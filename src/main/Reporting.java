89a5496c05f1344d31a93000939f981efc473a81
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-13 21:17:30 +0000

    Javadoc annotations for Reporting class

--- src/main/Reporting.java
+++ src/main/Reporting.java
@@ -4,15 +4,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Reporting represents the class that will contain all of existing
+ * Districts.Reporting will use these district to generate reports
+ * and perform basic data analysis.
+ */
 public class Reporting {
     private final List<District> districts = new ArrayList<>();
 
+    /**
+     * Construct an empty reporting object
+     */
     public Reporting() {}
 
+    /**
+     * Construct a reporting object with list of District
+     * @param districts the list of District that will be added to districts field
+     */
     public Reporting(List<District> districts) {
         this.districts.addAll(districts);
     }
 
+    /**
+     * Construct a reporting object with array of District
+     * @param districts array of District to be added to districts field
+     */
     public Reporting (District[] districts) {
         this(Arrays.asList(districts));
     }
@@ -24,14 +40,27 @@ public class Reporting {
                 '}';
     }
 
+    /**
+     *
+     * @return all districts in the districts field
+     */
     public List<District> getDistricts() {
         return this.districts;
     }
 
+    /**
+     * Adds one or more District objects to districts field
+     * @param districts District/s to be added to districts field
+     */
     public void addDistrict(District... districts) {
         this.districts.addAll(Arrays.asList(districts));
     }
 
+    /**
+     * Generate the greatest average value stolen in a district at a given year
+     * @param year the year wanted for the average
+     * @return greatest average value of incidents at a given year
+     */
     public District maxAverageValInDistAt(int year) {
         if (this.districts.size() == 0) {
             return null;
@@ -51,6 +80,10 @@ public class Reporting {
         return highest;
     }
 
+    /**
+     * Greatest value stolen in an Incident from all districts
+     * @return Incident with the most value stolen from all districts
+     */
     public Incident maxIncidentVal() {
         if (this.districts.size() == 0) {
             return null;
@@ -73,6 +106,11 @@ public class Reporting {
         return highest;
     }
 
+    /**
+     * Incidents with greater value than the given amount on every District
+     * @param amount amount to be used for comparison
+     * @return list of Incidents that exceed the amount given
+     */
     public List<Incident> getIncidentWithValGreaterThan(double amount) {
         List<Incident> incidentList = new ArrayList<>();
 
