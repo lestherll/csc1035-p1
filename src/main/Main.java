cdf9ba26fcbc3bd4b3d64e5f196ca1cca788effb
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-14 02:17:14 +0000

    Javadoc annotations for Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
+/**
+ * Integrates all of the classes and creates an interactive
+ * program for the user
+ */
 public class Main {
     public static void main(String[] args) {
         ReportingIO reportingIO = new ReportingIO();
