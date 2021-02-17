f9bca683c40f67c7fe5518f6cf9155110334f13e
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-02-17 23:20:10 +0000

    Modified inputMethods to not allow blanks or pure spaces

--- src/main/ReportingIO.java
+++ src/main/ReportingIO.java
@@ -19,60 +19,87 @@ public class ReportingIO {
 
     /**
      * This allows the user to enter integer inputs based on the minimum
-     * and maximum arguments that have been passed by the user
+     * and maximum arguments that have been passed by the user. Does not
+     * allow empty/blank values.
      * @param min this depicts the minimum integer input
      * @param max this depicts the maximum integer input
      * @param message the message shown when the program asks for input
      * @return the valid input by the user
      */
-    private int enterNum(int min, int max, String message) {
+    public int enterNum(int min, int max, String message) {
         int number;
+        String input;
+
         while (true){
             System.out.printf("\n%s: ", message);
-            try {
-                number = Integer.parseInt(this.scanner.next());
-                if ((number <= max) && (number >= min)) {
-                    break;
+            input = this.scanner.nextLine();
+            if (!input.isBlank()) {
+                try {
+                    number = Integer.parseInt(input);
+                    if ((number <= max) && (number >= min)) {
+                        break;
+                    }
+                    System.out.printf("%s Outside of range\n", number);
                 }
-                System.out.printf("%s Outside of range\n", number);
+                catch (NumberFormatException ignore) {
+                    System.out.println("Invalid input");
+                }
             }
-            catch (NumberFormatException ignore) {
-                System.out.println("Invalid input");
+            else {
+                System.out.println("Can't enter empty val");
             }
+
         }
-        this.scanner.nextLine();    // consume rest of the line
         return number;
     }
 
     /**
-     * A basic double input for the user
+     * A basic double input for the user. Does not allow empty/blank values.
      * @param message argument that the user must pass to be
      *                displayed when the program asks for input
      * @return validated input that the user will enter
      */
     private double enterDouble(String message) {
         double number;
+        String input;
 
         while (true) {
             System.out.printf("\n%s: ", message);
-            try {
-                number = Double.parseDouble(this.scanner.next());
-                break;
-            } catch (NumberFormatException ignore) {
-                System.out.println("Invalid input");
+            input = this.scanner.nextLine();
+            if (!input.isBlank()) {
+                try {
+                    number = Double.parseDouble(input);
+                    break;
+                } catch (NumberFormatException ignore) {
+                    System.out.println("Invalid input");
+                }
             }
+            else {
+                System.out.println("Can't enter empty val");
+            }
+
         }
         return number;
     }
 
     /**
-     * Allows the user to input string
+     * Allows the user to input string. Does not allow empty/blank values.
      * @param message the message shown when the program asks for input
      * @return the input that the user entered in string form
      */
     private String enterStr(String message) {
-        System.out.printf("\n%s: ", message);
-        return this.scanner.nextLine();
+        String input;
+        while (true) {
+            System.out.printf("\n%s: ", message);
+            input = this.scanner.nextLine();
+            if (!input.isBlank()) {
+                break;
+            }
+            else {
+                System.out.println("Can't enter empty value");
+            }
+        }
+        return input;
     }
 
     /**
